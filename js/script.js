ac581c3c2bca0c9bda0ff27f78d0e56ba351dc1a
Andrea030105 <tummoloandrea05@gmail.com>
2025-12-16 16:49:06 +0100

    fix-mounted

--- js/script.js
+++ js/script.js
@@ -9,15 +9,10 @@ createApp({
         }
     },
     mounted() {
-        this.generateEmail();
-    },
-    methods: {
-        generateEmail() {
-            for (let i = 0; i < 10; i++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    this.emailArray.push(result.data.response)
-                })
-            }
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.emailArray.push(result.data.response)
+            })
         }
     }
 }).mount('#app')
