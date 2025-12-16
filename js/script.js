005f75b62e9c5b6960f79407cdf3eaeb569d4104
Andrea030105 <tummoloandrea05@gmail.com>
2025-12-16 16:37:13 +0100

    ex-done

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@ const {
 createApp({
     data() {
         return {
-            ciao: "caio",
+            emailArray: [],
         }
     },
-
+    mounted() {
+        this.generateEmail();
+    },
+    methods: {
+        generateEmail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    this.emailArray.push(result.data.response)
+                })
+            }
+        }
+    }
 }).mount('#app')
